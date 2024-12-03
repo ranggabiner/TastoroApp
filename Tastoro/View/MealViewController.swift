1cc0519a8f18892176e94a4346e5ffd627526878
Rangga Biner <ranggabiner@gmail.com>
2024-12-04 05:04:25 +0700

    add: Search

--- Tastoro/View/MealViewController.swift
+++ Tastoro/View/MealViewController.swift
@@ -13,19 +13,39 @@ protocol MealViewProtocol: AnyObject {
     func showError(_ message: String)
 }
 
-class MealViewController: UIViewController, MealViewProtocol {
+class MealViewController: UIViewController, MealViewProtocol, UISearchBarDelegate {
     let tableView = UITableView()
     var presenter: MealPresenterProtocol?
     private var meals: [Meal] = []
     private let areaFilter = UISegmentedControl(items: ["All", "Indian", "Chinese", "Japanese", "French", "Moroccan"])
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search Meals"
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    private var filteredMeals: [Meal] = []
+    private var isSearching: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Choose Your Menu"
         view.backgroundColor = .white
         setupAreaFilter()
+        setupSearchBar()
         setupTableView()
         presenter?.viewDidLoad()
+    }
+    
+    private func setupSearchBar() {
+        searchBar.delegate = self
+        view.addSubview(searchBar)
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: areaFilter.bottomAnchor, constant: 8),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
     }
 
     private func setupAreaFilter() {
@@ -47,15 +67,14 @@ class MealViewController: UIViewController, MealViewProtocol {
         tableView.delegate = self
         tableView.register(MealTableViewCell.self, forCellReuseIdentifier: "MealCell")
         view.addSubview(tableView)
-
+        
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: areaFilter.bottomAnchor, constant: 8),
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 8),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
-    
     @objc private func areaFilterChanged() {
         let selectedArea = areaFilter.titleForSegment(at: areaFilter.selectedSegmentIndex) ?? "All"
         presenter?.filterMeals(by: selectedArea == "All" ? "" : selectedArea)
@@ -76,14 +95,14 @@ class MealViewController: UIViewController, MealViewProtocol {
 
 extension MealViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return meals.count
+        return isSearching ? filteredMeals.count : meals.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "MealCell", for: indexPath) as? MealTableViewCell else {
             return UITableViewCell()
         }
-        let meal = meals[indexPath.row]
+        let meal = isSearching ? filteredMeals[indexPath.row] : meals[indexPath.row]
         cell.mealLabel.text = meal.strMeal
         if let imageUrl = URL(string: meal.strMealThumb) {
             URLSession.shared.dataTask(with: imageUrl) { data, _, _ in
@@ -96,9 +115,30 @@ extension MealViewController: UITableViewDataSource, UITableViewDelegate {
         }
         return cell
     }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         presenter?.didSelectMeal(at: indexPath.row)
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            isSearching = false
+            filteredMeals = []
+        } else {
+            isSearching = true
+            filteredMeals = meals.filter { $0.strMeal.lowercased().contains(searchText.lowercased()) }
+        }
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        isSearching = false
+        filteredMeals = []
+        tableView.reloadData()
+        searchBar.resignFirstResponder()
+    }
+
     
 }
